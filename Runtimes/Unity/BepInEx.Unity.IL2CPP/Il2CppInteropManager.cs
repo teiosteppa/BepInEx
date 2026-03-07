e6afeacd3e30972ccbbff61fab018ccf5ac64104
teiosteppa <github@teiostep.com>
2026-03-07 14:27:44 -0800

    fix: properly patch assemblies before interop runtime starts

--- Runtimes/Unity/BepInEx.Unity.IL2CPP/Il2CppInteropManager.cs
+++ Runtimes/Unity/BepInEx.Unity.IL2CPP/Il2CppInteropManager.cs
@@ -283,6 +283,10 @@ internal static partial class Il2CppInteropManager
         AppDomain.CurrentDomain.AssemblyResolve += ResolveInteropAssemblies;
 
         GenerateInteropAssemblies();
+    }
+
+    public static void StartRuntime()
+    {
         var interopLogger = LoggerFactory.CreateLogger("Il2CppInterop");
 
         var unityVersion = UnityInfo.Version;
